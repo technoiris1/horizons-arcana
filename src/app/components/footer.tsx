36237193890e9305428db2f85f79c76632c2b359
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-03-30 20:05:05 +0530

    added a footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return (
+        <footer className="w-full text-center text-sm text-gray-500">
+            <img src={"/arcana-footer.png"} className="w-full pb-0"></img>
+        </footer>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "./components/hero";
+import Footer from "./components/footer";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <Hero />
+      <Footer />
     </div>
   );
 }
