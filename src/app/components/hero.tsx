efafae635d2b69a3440c50bf7052b6c414c98cc9
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-03-30 19:12:28 +0530

    made a basic hero with the hero image

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="w-full overflow-x-hidden">
+            <img src="/arcana-hero.png" alt="" className="w-full h-auto block" />
+        </div>
+    );
+}
