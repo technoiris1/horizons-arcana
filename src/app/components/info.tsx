5077b42a7edbcd7d57f18a883c74839907b36f42
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2026-04-22 21:46:50 +0530

    made the travel page proper

--- src/app/components/info.tsx
+++ src/app/components/info.tsx
@@ -15,7 +15,7 @@ export default function Info() {
     <section className={`${averiaSerif.className} bg-[#1b3364] px-6 pt-0 pb-24 sm:px-10 lg:px-16`}>
       <div className="mx-auto max-w-7xl">
 
-        {/* navbar with all the tabs */}
+        {/* navbar with all the tabs */}  
         <nav className="mt-0 flex flex-wrap items-center justify-center gap-x-2 gap-y-6 text-center pt-0" aria-label="Info sections">
           <button
             type="button"
@@ -259,7 +259,7 @@ Feel free to email us at <a href="mailto:arcana@horizons.hackclub.com" className
               <div className="space-y-10">
                 <div className="pb-2">
                   <p className="mt-4 max-w-4xl leading-8 text-[#6cc7ec] sm:text-xl">
-                    In order to qualify for Arcana, you'll need to spend X amount of hours working on one or multiple projects and ship it on the <a href="https://horizons.hackclub.com" target="_blank" className="event-link">horizons platform</a>. Once it's reviewed and approved, you can buy a ticket of the event from there for free!
+                    In order to qualify for Arcana, you'll need to spend <span className="event-link">35 hours</span> working on one or multiple projects and ship it on the <a href="https://horizons.hackclub.com" target="_blank" className="event-link">horizons platform</a>. Once it's reviewed and approved, you can buy a ticket of the event from there for free!
                   </p>
                 </div>
                 <div className="pb-2">
@@ -297,25 +297,58 @@ Feel free to email us at <a href="mailto:arcana@horizons.hackclub.com" className
               <h3
                 className="text-3xl font-black tracking-tight text-[#f9b6ff] sm:text-4xl"
               >
-                Travel Stipends
+                Travelling to Arcana
               </h3>
               <div className="space-y-10">
                 <div className="pb-2">
                   <p className="mt-4 max-w-4xl leading-8 text-[#6cc7ec] sm:text-xl">
-                    Our aim is to make this event accessible to everyone regardless of their financial or geographical background, so we will be providing travel stipends. You can get them by working extra hours over the event requirement from the Horizons shop at a rate of <span className="event-link">8.5$ for every hour </span> spent, which you can spend towards your flights/visa/passport etc.  <br />
-                    </p>
+                  Arcana is in Singapore, a small island nation in South-East Asia. Singapore is pretty well connected to the rest of the world, below you can find some information which can help you plan your travel-
+              </p>
                 </div>
                          <div className="pb-2">
                     <p
                     className="text-base font-bold text-[#f9b6ff] sm:text-2xl"
                   >
-                    Flying in and out of Singapore
+                    Travel Stipends
                   </p>
              
                   <p className="mt-4 max-w-4xl leading-8 text-[#6cc7ec] sm:text-lg">
-Singapore has only one main airport- <span className="event-link"> Singapore Changi</span>. You can request a staff pickup from the airport, if you do- staff will receive you and escort you to the venue. We will put out a form as we move closer to the event to request a staff pickup. 
-More details regarding when to fly in and out will be announced as we move closer to the event.
+Our aim is to make this event accessible to everyone regardless of their financial or geographical background, so we will provide travel stipends to whoever works for them. You can get the travel stipends from Horizons shop at the rate of 8.5$/hr, which you can spend towards your flights, visa etc.
                   </p>
+                </div>
+                <div className="pb-2">
+                  <p
+                  className="text-base font-bold text-[#f9b6ff] sm:text-2xl"
+                  >
+                  Flying in and out of Singapore
+                </p>
+             
+          <p className="mt-4 max-w-4xl leading-8 text-[#6cc7ec] sm:text-lg">
+Singapore has one main airport- Singapore Changi, it is one of the best airports in the world and is very well connected to the rest of the world. 
+You should try to fly in before the evening of 31st July and fly out anytime after the evening of 2nd August.
+    </p>
+                </div>
+                                <div className="pb-2">
+                  <p
+                  className="text-base font-bold text-[#f9b6ff] sm:text-2xl"
+                  >
+                  Visa
+                </p>
+             
+          <p className="mt-4 max-w-4xl leading-8 text-[#6cc7ec] sm:text-lg">
+Most nationalities can travel to Singapore visa-free for a certain number of days or can get a visa on arrival(VoA). If you require a visa to enter Singapore, we will try our best to help you out with it. More details surrounding the visa process will be announced as we move closer to the event.
+    </p>
+                </div>
+                <div className="pb-2">
+                  <p
+                  className="text-base font-bold text-[#f9b6ff] sm:text-2xl"
+                  >
+                  Airport Pickups
+                </p>
+             
+          <p className="mt-4 max-w-4xl leading-8 text-[#6cc7ec] sm:text-lg">
+Participants can request airport pickups, if requested- staff members will be there at the airport to pick them up and drop them to the venue. More details about this will be announced as we move closer to the event.
+  </p>
                 </div>
               </div>
             </div>
